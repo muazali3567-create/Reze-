8378cacfb17daedff4e26841c30e2e0eefca172e
moozy <muaz.ali3567@gmail.com>
2026-01-31 18:27:12 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-import express from "express";
-
+const express = require("express");
 const app = express();
+
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.send("Reze is online. Stop staring and say something.");
 });
 
-app.post("/chat", (req, res) => {
-  const userMessage = req.body.message || "";
+app.post("/talk", (req, res) => {
+  const userInput = req.body.message || "";
   res.json({
-    reply: `Hmph. "${userMessage}"? You better not be wasting my time.`
+    reply: `Hmph… "${userInput}"? You better not be wasting my time.`
   });
 });
 
